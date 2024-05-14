c6dced6a6aafd365c3e61a582ccb7456bdd7305d
webmaster lecarosgroup <webmaster@lecarosgroup.com>
2024-05-14 18:10:52 -0400

    Primer push prueba

---
+++ Contabilidad/Clases/Observacion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Contabilidad.Clases
+{
+    public class Observacion
+    {
+        public string fecha { get; set; }
+        public string nomUsuario { get; set; }
+        public string observaciones { get; set; }
+    }
+}
---
+++ Contabilidad/Clases/Pago.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Contabilidad.Clases
+{
+    public class Pago
+    {
+        public int idAsignacion { get; set; }
+        public int idSol { get; set; }
+        public string fechaTermino { get; set; }
+        public string rut { get; set; }
+        public int idProveedor { get; set; }
+        public string nomProveedor { get; set; }
+        public int idCotizacion { get; set; }
+        public int idProp { get; set; }
+        public int idContrato { get; set; }
+        public string propiedad { get; set; }
+        public string servicio { get; set; }
+        public string descripcion { get; set; }
+        public int total { get; set; }
+        public byte idEstado { get; set; }
+        public string fecEstado { get; set; }
+        public string estado { get; set; }
+        public string rutCta { get; set; }
+        public string nombreCta { get; set; }
+        public int idBanco { get; set; }
+        public int idTipoCta { get; set; }
+        public string numCta { get; set; }
+        public string nomArchFactura { get; set; }
+        public string numFactura { get; set; }
+        public int idFactura { get; set; }
+        public string para { get; set; }
+        public int id_tipo_proveedor { get; set; }
+        public string tipo_proveedor { get; set; }
+    }
+    public class listascombobox
+    {
+        public int id { get; set; }
+        public string descripcion { get; set; }
+        public string periodo_dia { get; set; }
+        public string periodo_mes { get; set; }
+    }
+    public class MiViewModel
+    {
+        public string Test { get; set; }
+    }
+}
